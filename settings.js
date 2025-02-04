86d53c2a4acb8068e59e81080e58e089639a2ee0
ZAYCRIMSON <154770373+ZayCrimson@users.noreply.github.com>
2025-02-04 10:11:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'AJAY',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "AJAY BOT",
+  botOwner: 'ZayCrimsons', // Your name
+  ownerNumber: '081358238538', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
